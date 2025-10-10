6272afe07128a394884d4fa3f3a6765a4b2af59e
Pablo Zallio <pablozalliodev@gmail.com>
2025-10-10 12:39:35 +0200

    [ADD] confirming changes and theme

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -18,7 +18,7 @@ function InputSearch({darkMode, valueInput, setValueInput}: InputSearchProps){
     return(
         <>
         <section className="flex justify-center py-9">
-           <section className={`flex w-[75%] lg:w-[60%] xl:w-[40%] px-2 rounded-full ${ darkMode ?'bg-[#e7e8fa]':'dark:bg-[#3c4096]'}`}>
+           <section className={`flex w-[75%] lg:w-[60%] xl:w-[40%] px-2 rounded-full ${ darkMode ?'bg-[#e7e8fa]':'bg-[#3c4096]'}`}>
             <img className='p-3' src={loupe} alt="Loupe image" />
             <input className={`w-full focus:outline-0 ${darkMode ?'placeholder-[#575f5e]' :''}`} type="text" name="" id="" placeholder='Search by title, year... ' onChange={displayBtn} value={valueInput}/>
             <img className={`${valueInput.length > 0 ? 'block' : 'hidden'}`} src={closeInput} alt="Close icon" onClick={resetInput}/>
